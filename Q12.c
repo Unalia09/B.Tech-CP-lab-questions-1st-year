f6b2aea4115300d9eddc3e651cf2035298d3a928
Unalia09 <147930477+Unalia09@users.noreply.github.com>
2024-04-28 20:03:51 +0530

    Create Q12.c

---
+++ Q12.c
@@ -0,0 +1,11 @@
+//program to compute simple interest 
+#include <stdio.h>
+
+int main() {
+      float P,R,T, SI;
+      P=12;
+      R=16;
+      T=14;
+      SI=(P*R*T)/100;
+      printf("The simple interest is %f", SI);
+}
