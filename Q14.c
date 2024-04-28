19b649bc989c3e817aada0e42bc5e61008ea2530
Unalia09 <147930477+Unalia09@users.noreply.github.com>
2024-04-28 20:09:39 +0530

    Create Q14.c

---
+++ Q14.c
@@ -0,0 +1,13 @@
+//program to swap two variables using a third variable
+#include <stdio.h>
+
+int main() {
+     int a=5,b=6,c;
+     c=a;
+     a=b;
+     b=c;
+     printf("The value of a and b is %d %d", a,b );
+     
+     return 0;
+     
+}
